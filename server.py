ce233f2bb07001e0b99984056eada1e57a187eff
lamtharnhantrakul <hanoi7@gmail.com>
2017-12-11 14:38:11 -0500

    first version working

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#from keras.models import load_model
+
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 from pythonosc import dispatcher
@@ -9,7 +9,16 @@ import time
 import numpy as np
 import threading
 from distutils.util import strtobool
-#import tensorflow as tf
+from keras.models import load_model
+import tensorflow as tf
+
+# Load a trained model
+graph = tf.get_default_graph() # hack around keras tensorflow in multiple threads
+# read https://github.com/fchollet/keras/issues/5896 for more information on the hack
+print("loading model...")
+with graph.as_default():
+    model = load_model('./models/test_model_30.h5')
+print("completed model...")
 
 # hack to make state machine in python server work
 state = 'listening'
@@ -27,6 +36,7 @@ def data_handler(unused_addr, args, *osc_args):
         _ = queue.popleft() # pop oldest value out of queue
         queue.append(data_point) # add newest value to queue
     print(len(queue))
+    sendUDPmsg("/queue_length", maxClient, int(len(queue)))
 
 def finger_touch_handler(unused_addr, args, *osc_args):
     # (args[0], args[1] = (queue, finger_down)
@@ -40,6 +50,7 @@ def finger_touch_handler(unused_addr, args, *osc_args):
     queue = args[0]
     while (len(queue) > 0):
         queue.popleft()
+    #sendUDPmsg("/queue_length", maxClient, int(len(queue)))
 
 def player_state_handler(unused_addr, *osc_args):
     # osc_args[0] = player_state (0 or 1) from Max/MSP
@@ -58,13 +69,66 @@ def sendUDPmsg(address,maxClient,*args):
     msg = msg.build()
     maxClient.send(msg)
 
+def prepSequence(init_sequence):
+    pattern = np.array(init_sequence)
+    return pattern
+
+def modelPredict(pattern):
+    x = np.reshape(pattern, (1, pattern.shape[0], pattern.shape[1]))
+    prediction = model.predict(x, verbose=0) # prediction is (1,3) -> needed for concatenating to next dataset
+    return prediction
+
+def appendIdx(idx, datum):
+    datum = np.insert(datum, 0, idx) # append index to datapoint
+    # `sendUDPmsg()` expects the data to already be strongly typed
+    #datum = [int(datum[0]), float(datum[1]), float(datum[2]), float(datum[3])]
+    return datum
+
+def sendValues(maxClient,prediction):
+    msg = osc_message_builder.OscMessageBuilder(address = '/prediction')
+    msg.add_arg(int(prediction[0])) # prepend with an index for Max/MSP `coll` object
+    msg.add_arg(float(prediction[1])) # x coor
+    msg.add_arg(float(prediction[2])) # y coor
+    msg.add_arg(float(prediction[3])) # pressure
+    msg = msg.build()
+    maxClient.send(msg)
+
+def genSendPredictions(init_sequence):
+    pattern = prepSequence(init_sequence)
+
+    start_time = time.time()
+    num_predictions = 100
+    ramp_length = 20
+    with graph.as_default():
+
+        for i in range(num_predictions):
+            prediction = modelPredict(pattern) # prediction is (1,3)
+            prediction_reshaped = np.squeeze(prediction)
+            datum = appendIdx(i, prediction_reshaped)
+            #print(datum)
+            #sendUDPmsg("/prediction", maxClient, datum)  # reshape to (3,) and send message via UDP immediatly as it is generated
+            sendValues(maxClient,datum)
+
+            pattern = np.concatenate((pattern, prediction), axis=0)
+            pattern = pattern[1:len(pattern),:]
+
+        (final_x, final_y, final_pressure) = prediction_reshaped
+        fade_pressures = np.linspace(final_pressure, 0.0, num=20)
+
+        for j in range(ramp_length):
+            ramp_values = np.array((final_x, final_y, fade_pressures[j]))
+            ramp_datum = appendIdx(j+num_predictions,ramp_values)
+            #sendUDPmsg("/prediction", maxClient, ramp_datum)
+            sendValues(maxClient,ramp_datum)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
 if __name__ == '__main__':
     # Define the data that will be manipulated by the handlers
     queue = deque()
     queue_limit = 40
     LSTM_lookback = 30
     finger_down = 0
-
 
     # Define the server -> Max/MSP port
     maxClient = udp_client.UDPClient('127.0.0.1', 8000)
@@ -84,6 +148,7 @@ if __name__ == '__main__':
     # Define data needed for the finite state machine
     init_seq = []
 
+    print(" ------ Start playing! -------")
     while True:
         #print(self.state)
         #print("player_state:", player_state)
@@ -98,6 +163,7 @@ if __name__ == '__main__':
         elif state == 'predicting':
 
             # predict
+            genSendPredictions(init_seq)
             init_seq = []  # clear the init_seq
             player_state = 1
             sendUDPmsg("/player_state", maxClient, int(player_state))
@@ -105,8 +171,10 @@ if __name__ == '__main__':
             print("switched to playing state")
 
         elif state == 'playing':
-            # wait for `player_state_handler()` to get out of this loop
-
+            '''
+            the `player_state_handler()` breaks out of this loop when it receives
+            a command from Max/MSP that the player has finished playing the sequence
+            '''
 
             if len(queue) > 10:
                 # This must be an interruption
